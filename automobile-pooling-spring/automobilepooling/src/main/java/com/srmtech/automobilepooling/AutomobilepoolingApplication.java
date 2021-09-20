e59216c5f862ea5748aa043bc8215c647965e24f
Adharsh <81523460+adharshrj@users.noreply.github.com>
2021-09-20 17:03:22 +0530

    up

---
+++ automobile-pooling-spring/automobilepooling/src/main/java/com/srmtech/automobilepooling/AutomobilepoolingApplication.java
@@ -0,0 +1,13 @@
+package com.srmtech.automobilepooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobilepoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobilepoolingApplication.class, args);
+	}
+
+}
---
+++ automobile-pooling-spring/automobilepooling/src/test/java/com/srmtech/automobilepooling/AutomobilepoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srmtech.automobilepooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobilepoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
