0ad6fe2785bff96ba38538db3c8542602eccce1f
Krithiga p <91064941+Krithigasrm@users.noreply.github.com>
2021-10-05 12:52:55 +0530

    Add files via upload

---
+++ carpooling-user-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './Components/User';
+import User1 from './Components/User1';
+import Map from './Components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <User/>
+      {/* <Map/> */}
+    </div>
+  );
+}
+
+export default App;
