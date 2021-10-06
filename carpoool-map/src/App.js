43da2825a2965cac5608b83d3e114766300400a5
Krithiga p <91064941+Krithigasrm@users.noreply.github.com>
2021-10-06 22:06:12 +0530

    Add files via upload

---
+++ carpoool-map/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './Components/User';
+import Map from './Components/Map';
+import Home from './Components/Home';
+import Map2 from './Components/Map2';
+import Map3 from './Components/Map3';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <User/> */}
+      {/* <Map/> */}
+    {/* <Map2/> */}
+    <Map3/>
+      {/* <Home/> */}
+    </div>
+  );
+}
+
+export default App;
