5474ff285d550601e05d2e78714f54b55c65c063
Divyasrm <divyap@srmtech.com>
2021-10-21 13:28:06 +0530

    dashboard

--- automobile-pooling/automobilepooling-frontend/src/components/dashboard/passenger/PassengerTopbar.js
+++ automobile-pooling/automobilepooling-frontend/src/components/dashboard/passenger/PassengerTopbar.js
@@ -10,18 +10,11 @@ export default function PassengerTopbar() {
           <span className="logo">SRM Automobile Pooling</span>
         </div>
         <div className="topRight">
-          <div className="topbarIconContainer">
-            <NotificationsNone />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Language />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Settings />
-          </div>
           <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <n/>
+          Preethi
+          
+        
         </div>
       </div>
     </div>
