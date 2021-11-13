39126d4a931df443420861d6f92d2114f1fc6017
Krithiga p <krithigap@srmtech.com>
2021-11-13 20:10:29 +0530

    update

--- automobile-pooling-bk/pooling/automobilepooling-fe/src/owner/components/widgetLg/WidgetLg.js
+++ automobile-pooling-bk/pooling/automobilepooling-fe/src/owner/components/widgetLg/WidgetLg.js
@@ -1,84 +1,62 @@
 import "./widgetLg.css";
-import { Visibility } from "@material-ui/icons";
-import { DataGrid } from "@material-ui/data-grid";
 import axios from 'axios';
 import authHeader from '../../../services/authHeader';
-import { useHistory } from 'react-router-dom';
 import { useEffect,useState } from "react";
-import * as React from 'react';
 
 export default function WidgetLg() {
+  const config = {
+    headers: authHeader() 
+  };
 
-  const[result,setValues]=useState([]);
-
- 
+  const[res,setValues]=useState([]);
 
   const loadUser = async () => {
-    const result = await axios.get(`http://localhost:9000/api/user/owner`,config);
-    setValues(result.data);
-    console.log(result.data)
+    const res = await axios.get(`http://localhost:9000/api/getride`,config);
+    setValues(res.data);
+    console.log(res.data)
   };
   useEffect(() => {
    console.log("useeffect");
-    loadUser();
-    
-   
+    loadUser(); 
 }, []);
-
-
-
-const config = {
-  headers: authHeader()
-};
-
-const hist = useHistory();
-  
-  const Button = ({ type }) => {
-    return <button className={"widgetLgButton " + type}>{type}</button>;
-  };
-
-
-   
   return (
-    <div className="widgetLg">
-      <h3 className="widgetLgTitle">Owner Details</h3>
-      <table className="widgetLgTable">
-        <tr className="widgetLgTr">
-          <th className="widgetLgTh">Name</th>
-          <th className="widgetLgTh">Source</th>
-          <th className="widgetLgTh">Destination</th>
-          <th className="widgetLgTh">Mobile Number</th>
-          <th className="widgetLgTh">Details</th>
-          <th className="widgetLgTh">Ride Request</th>
-        </tr>
-          
-        {result.map((x) => {
-          return <tr className="widgetLgTr">
-          <th className="widgetLgGet" ><label key={x.id}>{x.firstname+" "}{x.lastname}</label></th>
-          <th className="widgetLgGet" ><label key={x.id}>{x.source}</label></th>
-          <th className="widgetLgGet"><label key={x.id}>{x.destination}</label></th>
-          <th className="widgetLgGet"><label key={x.id}>{x.mobile}</label></th>
-          
-          <th className="widgetLgStatus">
-            <button  className="widgetSmButton" onClick={() => hist.push("/view")}>
-              <Visibility className="widgetSmIcon" />
-              View 
-              </button>
-          </th>
-
-          <th className="widgetLgButton">
-            <Button type="Request" />
-          </th>
-
-          
-     
-          </tr>
-
-           
-        })} 
-            
-      </table>
-
-    </div>
+    
+    <div class="col-lg-8 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Passengers Details</h4>
+                   
+                    <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th> Name </th>
+                            <th> Designation </th>
+                            <th> Source </th>
+                            <th> Destination </th>
+                            <th> Mobile </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        {res.map((x) => {
+                return <tr className="widgetLgTr">
+                <th >
+                 
+                  <span className="widgetLgName">{x.passenger.firstname+" "}{x.passenger.lastname}</span>
+                </th>
+                <th className="widgetLgName">{x.passenger.designation}</th>
+                <th className="widgetLgName">{x.passenger.source}</th>
+                <th className="widgetLgName">{x.passenger.destination}</th>
+                <th className="widgetLgName">{x.passenger.mobile}</th>
+                
+               
+              </tr>
+              })}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
   );
 }
